5beea7d3ae779c30ad1a522caff356d0ae232545
Devin Butts <devinmbutts@gmail.com>
2024-08-27 21:27:28 -0400

    test servo looks

--- Robot/first-test/first_test.py
+++ Robot/first-test/first_test.py
@@ -7,8 +7,8 @@ import redis
 r = redis.Redis(host='192.168.1.250', port=6379, db=0)
 
 # Initialize the current servo positions
-servo1_position = 1500  # Default neutral position
-servo2_position = 2000  # Default neutral position
+servo1_position = 2000  # Default neutral position
+servo2_position = 1500  # Default neutral position
 
 # Define the step size for each movement
 servo_step = 50
